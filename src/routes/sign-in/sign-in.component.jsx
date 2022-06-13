c8f893bb230978ca024ca263eab4559d2775ef75
Miroslav Kodžoman <klop@neyho.com>
2022-06-13 14:30:58 +0200

    Commented out Google redirect login functionality

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,40 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+// import { useEffect } from 'react';
+// import { getRedirectResult } from 'firebase/auth'
+
+import { 
+  auth,
+  signInWithGooglePopup,
+  signInWithGoogleRedirect, 
+  createUserDocumentFromAuth 
+} from '../../utils/firebase/firebase.utils'
  
 const SignIn = () => {
+
+  // useEffect(() => {
+  //   async function redirectLogin() {
+  //     const response = await getRedirectResult(auth);
+  //     if(response) {
+  //       const userDocRef = await createUserDocumentFromAuth(response.user)
+  //     }
+  //   }
+  //   redirectLogin()
+  // }, [])
 
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user)
   }
 
+  const logGoogleRedirectUser = async () => {
+    const { user } = await signInWithGoogleRedirect();
+    console.log(user)
+  }
+
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign In With Google Popup</button>
+      {/*<button onClick={logGoogleRedirectUser}>Sign In With Google Redirect</button>*/}
     </div>
   )
 }
