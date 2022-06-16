eec7a25b41c41d344d55e96995afc25428f1c958
Miroslav Kodžoman <klop@neyho.com>
2022-06-16 14:13:44 +0200

    Created checkout page along with checkout items and checout cart funcionality

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -2,9 +2,9 @@ import './cart-item.styles.scss'
 
 const CartItem = ({cartItem}) => {
 
-  const { name, imageUrl, quantity, price } = cartItem
+  const { id, name, imageUrl, quantity, price } = cartItem
   return (
-    <div className='cart-item-container'>
+    <div key={id} className='cart-item-container'>
       <img src={imageUrl} alt={`${name}`}/>
       <div className='item-details'>
         <span className='name'>{name}</span>
