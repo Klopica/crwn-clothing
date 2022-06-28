aee4b626e89d0cdd3b0e27794dec8c9f58203b99
Miroslav Kodžoman <klop@neyho.com>
2022-06-28 15:27:55 +0200

    Select current user. Move to adding reducers to Categories

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,15 @@
 import { Fragment, useContext } from 'react'
-import {Outlet, Link} from 'react-router-dom'
+import {Outlet} from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
 
 import CartIcon from '../../components/cart-icon/cart-icon.component'
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
 
-import { UserContext } from '../../contexts/user.context'
 import { CartContext } from '../../contexts/cart.context'
+
+// import { selectCurrentUser } from '../../store/user/user.selector' // Ne radi
 
 import { signOutUser } from '../../utils/firebase/firebase.utils'
 // import './navigation.styles.scss'
@@ -17,9 +19,13 @@ import {
   NavLinks, 
   NavLink 
 } from './navigation.styles'
+import { selectCurrentUser } from '../../store/user/user.selector'
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext)
+
+  // const currentUser = useSelector(selectCurrentUser)
+  const currentUser = useSelector((state) => state.user.currentUser)
+
   const { isCartOpen } = useContext(CartContext)
 
   return (
