0cf8bd21a77373599d4017b28a6058fd1fe4f7ce
Miroslav Kodžoman <klop@neyho.com>
2022-06-29 15:46:01 +0200

    Migrate Cart Context To Redux

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,15 +1,16 @@
-import { useContext } from 'react'
-
-// import { ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
-
-import { CartContext } from '../../contexts/cart.context'
-
+import { useDispatch, useSelector } from 'react-redux'
 import { CartIconContainer, ShoppingIcon, ItemCount } from './cart-icon.styles.jsx'
 
-const CartIcon = () => {
+import { selectCartCount, selectIsCartOpen } from '../../store/cart/cart.selector.js'
+import { setIsCartOpen } from '../../store/cart/cart.action.js'
 
-  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext)
-  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen)
+const CartIcon = () => {
+  const dispatch = useDispatch();
+  // const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext)
+  const cartCount = useSelector(selectCartCount);
+  const isCartOpen = useSelector(selectIsCartOpen)
+  
+  const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
 
   return (
     <CartIconContainer onClick={toggleIsCartOpen}>
--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,3 +1,6 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { selectCartItems } from '../../store/cart/cart.selector'
+import { addItemToCart, clearItemFromCart, removeItemFromCart } from '../../store/cart/cart.action'
 import {
   CheckoutItemContainer, 
   ImageContainer, 
@@ -8,17 +11,16 @@ import {
   RemoveButton
 } from './checkout-item.styles'
 
-import { useContext } from 'react'
-import { CartContext } from '../../contexts/cart.context'
 
 const CheckoutItem = ( {cartItem} ) => {
-  const { name, imageUrl, quantity, price } = cartItem
 
-  const { clearItemFromCart, addItemToCart, removeItemFromCart } = useContext(CartContext)
+  const {name, imageUrl, price, quantity} = cartItem;
+  const cartItems = useSelector(selectCartItems);
+  const dispatch = useDispatch();
 
-  const clearItemHandler = () => clearItemFromCart(cartItem)
-  const addItemHandler = () => addItemToCart(cartItem)
-  const removeItemHandler = () => removeItemFromCart(cartItem)
+  const clearItemHandler = () => dispatch(clearItemFromCart(cartItems, cartItem));
+  const addItemHandler = () => dispatch(addItemToCart(cartItems, cartItem));
+  const removeItemHandler = () => dispatch(removeItemFromCart(cartItems, cartItem));
 
   return (
     <CheckoutItemContainer>
