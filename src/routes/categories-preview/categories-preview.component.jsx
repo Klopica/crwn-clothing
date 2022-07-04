da4b139a26011b57efaf6ec8f042989066b031c7
Miroslav Kodžoman <klop@neyho.com>
2022-07-04 11:42:10 +0200

    Redux Thunk

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,21 +1,30 @@
-import { Fragment } from 'react'
-import { useSelector } from 'react-redux'
-import CategoryPreview from '../../components/category-preview/category-preview.component'
-import { selectCategoriesMap } from '../../store/categories/category.selector'
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import CategoryPreview from '../../components/category-preview/category-preview.component';
+import Spinner from '../../components/spinner/spinner.component';
+import { selectCategoriesMap, selectCategoriesIsLoading } from '../../store/categories/category.selector';
 
 const CategoriesPreview = () => {
 
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   // console.log('CM: ', categoriesMap)
   return (
     <Fragment>
-    {Object.keys(categoriesMap).map((title) => {
-      const products = categoriesMap[title];
-      // console.log(products)
-      return (
-        <CategoryPreview key={title} title={title} products={products} />
+    {
+      isLoading ? (
+        <Spinner />
+      ) : (
+        Object.keys(categoriesMap).map((title) => {
+          const products = categoriesMap[title];
+          // console.log(products)
+          return (
+            <CategoryPreview key={title} title={title} products={products} />
+          )
+        })
       )
-    })}
+      
+    }
     </Fragment>
   )
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,26 +1,37 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-// import logger from 'redux-logger';
-
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import { rootReducer } from './root-reducer';
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if(!action.type) {
-    return next(action);
-  }
-
-  console.log('type', action.type);
-  console.log('payload', action.payload);
-  console.log('current state: ', store.getState());
-
-  next(action);
-
-  console.log('next state: ', store.getState());
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
 }
 
-const middleWares = [process.env.NODE_ENV === 'development' && loggerMiddleware].filter(
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(
   Boolean
 );
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+// console.log('ENV::: ', process.env.NODE_ENV)
+// This is to activate REDUX TOOLS in browser
+// TODO: REDUX tools in browser are still not activated, debug and refactor
+const composeEnhancer = 
+  (process.env.NODE_ENV !== 'production' && 
+  window && 
+  window.__REDUX_DEVTOOLS_EXTENSIONS_COMPOSE__) || 
+compose;
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(
+  persistedReducer, 
+  undefined, 
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
